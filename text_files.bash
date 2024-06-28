0a896c4d58c1d7aa76b4834e8270c95ddb82f5d4
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-06-28 22:39:37 +0100

    Add commands for working with text

--- text_files.bash
+++ text_files.bash
@@ -30,3 +30,46 @@ cat > savetofile.txt
 
 # force read from file instead of stdin and save to new file
 tr 'a-z' 'A-Z' < inputile.txt > outputfile.txt # tr-anslates lovercase to uppercase
+
+#-------------------------------------------
+# sorting
+head -5 /etc/passwd > passwords.txt
+sort passwords.txt # will send sorted file contents to stdout (file unchanged)
+# output data delimited by a collon (-t:) reverse (-r) sorted by a numerical (-n) 3rd kolumn (-k3)
+sort -t: -r -n -k3 passwords.txt 
+
+cat >os.txt # writes stdin to file
+1970,Unix,Richie
+1987,Minix,Tanenbaum
+1970,Unix,Thompson
+1991,Linux,Torvalds
+# sort  data delimited by comma (-t,) by column2, then by numerical column1, then by column3
+sort -t, -k2 -k1n -k3 os.txt
+
+#-------------------------------------------
+# file stats - word count
+wc /etc/passwd* # output file stats and total if more than one file
+# number of lines (-l), words (-w), bytes (-c), file name
+# count lines of output 
+ls /etc | wc -l
+
+#-------------------------------------------
+# output partial content
+# 'cut' out and display columns (file unchanged)
+cut -d: -f1,5-7 passwords.txt # from file delimited by colon (-d:) out put fields 1 and 5 to 7
+
+ls -l | cut -c1-11,50- # from file list display characters (-c) 1 to 11 and 50 till end
+
+#-------------------------------------------
+# find/filter for - grep
+ls -lAh /path | grep keyword # find files with keyword in file name in /path
+grep -c network /etc/passwd # count the number of occurances of "network" in passwd file
+grep -n network /etc/passwd # output occurances of "network" with line numbers from original file (-n)
+grep -v sys /etc/passwd # output NON-occurances (-v) of "sys"
+grep -i serv /etc/passwd # output occurance of "serv" case-insensitively (i.e. service and Server)
+grep -iw network /etc/passwd # output lines where ""
+grep ':...:' /etc/passwd # output all lines with a three number port
+grep [^0-z:/] /etc/passwd # output lines that have something other than any numbers, any letters, a collon or a slash
+grep '/r[ou]*' /etc/passwd # output lines containing "/r" followed by zero or more occurances of  o or u, i.e. /root /run /resolve
+grep 'ee*' /etc/passwd # output all ines that have at least ONE "e" character (since * is ZERO or more)
+grep -E '/s?bin/' /etc/passwd # output all lines that have zero or one "s" in the phrase, i.e. "sbin" and "bin", using Extended RegEx (-E)
