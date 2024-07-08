28ca695cbf2675d49e3a3edadcccbb0c605f1a24
VikSil <viktoria.sudraba@gmail.com>
2024-07-08 14:17:50 +0100

    Separate task description for Challenge Lab 1

--- Cisco_NDG_Essentials_Labs/challenge_lab_1.sh
+++ Cisco_NDG_Essentials_Labs/challenge_lab_1.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
-: <<'OBJECTIVES'
-
-    1) Create a directory at the root (/) of the file system for three departments: Engineering, Sales and IS. Each directory name should reflect the department name that will use the directory.
-
-    2) Create a group for each department. This name should reflect the department name that the group will be assigned.
-
-    3) Create an administrative user for each of the departments.
-        * The user will have a Bash login shell.
-        * The user will belong to the respective group for each department. This will need to be the user’s primary group.
-
-    4) Create two additional users for each department.
-        * The users will have a Bash login shell.
-        * The users will belong to their respective group for each department. This will need to be the user’s primary group.
-
-    5) For security reasons, the following modifications will need to be made to each of the departments' respective directories:
-        * Ensure that the owner of each of the directories is the department administrator and the group ownership is the group for each department.
-        * The department administrator will have full access to their respective department directories.
-        * Ensure that only the owner of a file in the department’s directory can delete the file. The user will also have ownership of their respective department folders.
-        * Normal users in each department will have full access (Read, Write and Execute) to their respective department folders.
-        * The department folders will ONLY be accessible by users/administrators in each of the respective departments. Ensure that no one else will have permissions to the folders.
-
-    6) Create a document in each of the department directories.
-        * The ownerships on this file will be the same as the directory it is located in.
-        * The document should contain only one line of text that states, “This file contains confidential information for the department.”
-        * This file can be read by any user in the department, but can only be modified by the department administrator. No one else has permissions to this file.
-
-OBJECTIVES
 
 DEPARTMENTS="engineering sales IS"
 
