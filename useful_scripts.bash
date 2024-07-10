12f1c49576e4e1ed0b764cfe59ff10fedd22a58e
VikSil <viktoria.sudraba@gmail.com>
2024-07-10 12:36:13 +0100

    Update useful_scripts

--- useful_scripts.bash
+++ useful_scripts.bash
@@ -9,3 +9,8 @@ ls | nl
 
 #-------------------------------------------
 sleep 60 # sleep for a minute
+
+#-------------------------------------------
+# Update VS Code
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O /tmp/code_latest_amd64.deb
+sudo dpkg -i /tmp/code_latest_amd64.deb
